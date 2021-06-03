dfad3c5bba31c38709e657c2d1810bad9479e50b
NoamRault <85115994+NoamRault@users.noreply.github.com>
2021-06-03 22:09:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
-const prefix = "!";
+require('discord-reply');
+require('dotenv').config();
+const prefix = ".";
 const client = new Discord.Client({
     presence: {
         status: 'online',
         activity: {
-            name: '!bestwaifu',
+            name: '.help',
             type: 'PLAYING',
         },
     },
@@ -15,23 +17,42 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.author.bot) return;
-    if (!msg.content.startsWith(prefix)) return;
-  
-    const command = msg.content.slice(prefix.length).toLowerCase();
+    if (msg.content === '-feur') reponse(msg, 'Ta gueule');
 
-    if (command === 'bestwaifu') {
-        const Urls = [
-            'https://static.wikia.nocookie.net/tora-dora/images/8/82/E17_-_9.png/revision/latest?cb=20160728131220',
-            'https://www.nautiljon.com/images/perso/00/61/taiga_aisaka_4516.jpg',
-            'https://i.pinimg.com/originals/3e/2c/db/3e2cdb504a1d1cd814885ebdf971c6b8.jpg',
-            'https://i.pinimg.com/originals/0d/d3/ac/0dd3ac5cac1cd782e0937acc720c5e9e.jpg',
-            'https://i.ytimg.com/vi/aozedrjkMdo/maxresdefault.jpg'
-        ];
-        
-        const url = Math.floor(Math.random() * Urls.length);
-        msg.channel.send('Taiga Best Waifu ' + Urls[url]);
+    if (msg.content.startsWith(prefix)) { // message commence par un !
+        const command = msg.content.slice(prefix.length).toLowerCase();
+
+        if (command === 'help') help(msg);
+        if (command === 'bestwaifu') reponse(msg, 'Taiga Best Waifu');
+        if (command === 'spamtaiga') {
+            let string = '<:Taiga:850087986036867092>';
+            for (let i = 0; i < 10 + Math.floor(Math.random() * 5); i++) {
+                string += ' <:Taiga:850087986036867092>';
+            }
+            msg.channel.send(string);
+        }
     }
 });
+
+function help(msg) {
+    const string = "```Commandes :\n"
+                    + "  .bestwaifu\n"
+                    + "  .spamtaiga\n"
+                    + "```";
+    msg.lineReply(string);
+}
+
+function reponse(msg, string) {
+    const Urls = [
+        'https://static.wikia.nocookie.net/tora-dora/images/8/82/E17_-_9.png/revision/latest?cb=20160728131220',
+        'https://www.nautiljon.com/images/perso/00/61/taiga_aisaka_4516.jpg',
+        'https://i.pinimg.com/originals/3e/2c/db/3e2cdb504a1d1cd814885ebdf971c6b8.jpg',
+        'https://i.pinimg.com/originals/0d/d3/ac/0dd3ac5cac1cd782e0937acc720c5e9e.jpg',
+        'https://i.ytimg.com/vi/aozedrjkMdo/maxresdefault.jpg'
+    ];
+    
+    const url = Math.floor(Math.random() * Urls.length);
+    msg.lineReply(string + ' ' + Urls[url] + ' <:Taiga:850087986036867092>');
+}
 
 client.login(process.env.TOKEN);
