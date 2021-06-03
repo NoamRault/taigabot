b1543c67f68bebc0ad508772a3648947030df4fb
NoamRault <85115994+NoamRault@users.noreply.github.com>
2021-06-03 17:57:54 +0200

    Update files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "!";
-import randomInt from 'random-int';
+const randomInt = require('random-int');
 
 client.on('ready', () => {
     client.user.setPresence({
